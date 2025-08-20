35c817a5d34889855efcbd850d5453098d333f40
Zennisch <snowy.sunny.dazing.us@gmail.com>
2025-08-21 00:28:48 +0900

    fix: preflight request CORS block

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -8,10 +8,16 @@ import (
 
 func SetupDefaultRoutes(router *gin.Engine) {
 	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := c.Request.Header.Get("Origin")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
 
 		c.Next()
 	})
